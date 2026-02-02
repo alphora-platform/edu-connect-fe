7229f527e0c6aa24d94842d4289fc74b350de0e0
TVan <hothithanhvan.dev@gmail.com>
2026-02-02 23:47:02 +0700

    feat(sign-up): implement sign-up page components and update styles

---
+++ src/routes/_auth/sign-up/_components/Divider.tsx
@@ -0,0 +1,11 @@
+export default function Devider() {
+  return (
+    <div className='relative flex py-6 items-center '>
+      <div className='grow border-t border-[#e5e7eb] dark:border-[#2a3e47]'></div>
+      <span className='shrink-0 mx-4 text-lg text-text-muted dark:text-slate-500'>
+        Or sign up with
+      </span>
+      <div className='grow border-t border-[#e5e7eb] dark:border-[#2a3e47]'></div>
+    </div>
+  )
+}
